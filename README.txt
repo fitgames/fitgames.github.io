4f0da356e688ca2895752c4525fe39b55e7f0f5a
Felipe Ramos <felipe98@users.noreply.github.com>
2018-03-19 00:21:54 -0500

    Updates

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/paulhbarker/pen/apvGdv.
+A Pen created at CodePen.io. You can find this one at http://codepen.io/mo7hamed/pen/dRoMwo.
 
- Column-based flexbox timeline layout. The goal is to have clean, semantic HTML while creating a (somewhat complex) timeline-looking layout.
+ 
---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+var downArrow = document.getElementById("btn1");
+var upArrow = document.getElementById("btn2");
+
+downArrow.onclick = function () {
+    'use strict';
+    document.getElementById("first-list").style = "top:-620px";
+    document.getElementById("second-list").style = "top:-620px";
+    downArrow.style = "display:none";
+    upArrow.style = "display:block";
+};
+
+upArrow.onclick = function () {
+    'use strict';
+    document.getElementById("first-list").style = "top:0";
+    document.getElementById("second-list").style = "top:80px";
+    upArrow.style = "display:none";
+    downArrow.style = "display:block";
+};
